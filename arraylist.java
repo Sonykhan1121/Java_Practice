4d7e25461b2c90a0168b4587cafcf0b79e49528f
Sidratul Montaha <sonykhan1121@gmail.com>
2024-12-16 19:05:24 +0600

    ArrayList or dynamic array

---
+++ arraylist.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> ar = new ArrayList<>();
+        ar.add(10);
+        ar.add(20);
+        ar.add(30);
+
+        System.out.println(ar);
+
+    }
+}
