cfde54ba2cd557ac3c55faa390fd93de8c957f03
Sidratul Montaha <sonykhan1121@gmail.com>
2024-12-16 13:43:55 +0600

    end of file

---
+++ end_of.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class end_of {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        while(input.hasNext()) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            System.out.println(a+" "+b);
+    }
+
+}
+}
