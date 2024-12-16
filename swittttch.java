66982466a307ef8f213cac98ef0298dde215c4fa
Sidratul Montaha <sonykhan1121@gmail.com>
2024-12-16 15:55:27 +0600

    Switch case

---
+++ swittttch.java
@@ -0,0 +1,17 @@
+public class swittttch {
+    public static void main(String[] args) {
+        int n = 9;
+        switch (n) {
+            case 5:
+                System.out.println("It is five");
+                break;
+            case 7:
+                System.out.println("It is seven");
+            case 8:
+                System.out.println("It is eight");
+            default:
+            System.out.println("Just a normal number : "+n);
+                break;
+        }
+    }
+}
