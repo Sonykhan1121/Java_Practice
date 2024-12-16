58ae3c6128370a36104d4ef4e454b54664c5fa25
Sidratul Montaha <sonykhan1121@gmail.com>
2024-12-16 19:33:40 +0600

    Simple calculator

---
+++ C_Simple_Calculator.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class C_Simple_Calculator{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        long a ,b;
+        a = input.nextLong();
+        b  = input.nextLong();
+
+        System.out.println(a+" + "+b+" = "+(a+b));
+        System.out.println(a+" * "+b+" = "+(a*b));
+        System.out.println(a+" - "+b+" = "+(a-b));
+
+    }
+}
