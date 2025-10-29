15d25648ac5f67635867379f6624350e3d7dcd1a
Sidratul Montaha <sonykhan1121@gmail.com>
2025-10-29 23:16:18 +0600

    restart my java journey

--- print.java
+++ print.java
@@ -1,5 +1,5 @@
 public class print {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! restart my journey");
     }
 }
