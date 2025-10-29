f435df3360ef3e2da8e130fbe5e3ec6a1051604e
Sidratul Montaha <sonykhan1121@gmail.com>
2025-10-29 23:18:00 +0600

    remove previous files and restart my java journey

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a sample Java program.");
+        System.out.println("I clear my depository.");
+    }
+}
--- set_precision.java
+++
@@ -1,8 +0,0 @@
-public class set_precision {
-    public static void main(String[] args) {
-        
-        double pi = 3.14159;
-        System.out.printf("pi = %.4f", pi);
-
-    }
-}
